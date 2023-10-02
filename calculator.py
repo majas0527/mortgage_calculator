59d5aed4cb9dd3a5dbef7193948939774d13ac9a
Maja S. <majastanojevic2705@gmail.com>
2023-10-02 13:46:34 +0100

    edit, seventh version

--- calculator.py
+++ calculator.py
@@ -22,6 +22,9 @@ mp2 = FixedMortgageProduct(**params2)
 
 st.write(f'If your interest rate increased by 3%, your new monthly payment would be £{round(mp2.get_pmt()):,}.')
 
+
+
+
 payment = st.slider("How much is your monthly payment?", 50,1000,200,10)
 rate = st.slider("What is your interest rate?", 0.01,0.1,0.03,0.01)
 years = st.slider("How many years will it take to pay back your mortgage?", 5,20,10)
@@ -31,4 +34,4 @@ params3 = {'pmt': payment, 'rate': rate, 'nper': years}
 mp3 = FixedMortgageProduct(**params3)
 
 st.write(f'You can make a monthly payment of £{payment} at an interest rate of {round(rate*100)}% over {years} years.')
-st.write(f'Your loan is £{round(mp.get_pv()):,}.')
+st.write(f'Your loan is £{round(mp3.get_pv()):,}.')
